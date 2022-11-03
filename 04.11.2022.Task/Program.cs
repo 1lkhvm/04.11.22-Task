8636e639809ee06a38f59c11af73b6b3eb518935
1lkhvm <tu6wkkvyr@code.edu.az>
2022-11-04 02:03:05 +0400

    Complete

---
+++ 04.11.2022.Task/Program.cs
@@ -0,0 +1,58 @@
+﻿////1 false
+
+//int eded1 = 200;
+//int eded2 = 1;
+//int toplam = 0;
+
+//if (eded1 > 50 && eded2 > 50)
+//    {
+//        toplam = eded1 + eded2;
+//            Console.WriteLine(toplam);
+//    }
+    
+
+//   if (eded1 < 50 && eded2 > 50) ;
+//{ 
+//        Console.WriteLine("eded1=" +eded1 );
+//            Console.WriteLine("eded2=" + eded2);
+//                Console.WriteLine("Daxil edilen ededlerden biri 50den kicikdir");
+
+//}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////1 True
+//int eded1 = 200;
+//int eded2= 100;
+//int toplam = 0;
+
+//if (eded1 > 50 && eded2 > 50)
+//{
+//    toplam = eded1 + eded2;
+//    Console.WriteLine(toplam);
+//}
+
+
+//if (eded1 < 50 && eded2 > 50) ;
+//{
+//    Console.WriteLine("eded1=" + eded1);
+//    Console.WriteLine("eded2=" + eded2);
+//    Console.WriteLine("Daxil edilen ededlerden biri 50den kicikdir");
+//}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////2
+//int[] Arr = { 1, 1, 4, 2, 5, 5, 3, 6, 2, 1, 8 };
+//int output = 0;
+// for (int i = 0; i < Arr.Length; i++)
+//{
+//          for (int a = i + 1; a < Arr.Length; a++)
+//          {
+//                  if (Arr[i] == Arr[a])
+//                  {                       
+//            output++;
+//            Console.WriteLine(Arr[a] + "==" + Arr[a]);
+
+//                  }   
+//          }
+//}
+//Console.WriteLine("Tekrarlanmis reqemlerin sayi-"  + output);
---
+++ Task C#,JavaScript/04.11.2022 Javascript/code.js
@@ -0,0 +1,92 @@
+// ////1 false
+
+// var eded1 = 200;
+// var eded2 = 1;
+// var toplam = 0;
+
+// if (eded1 > 50 && eded2 > 50)
+//    {
+//        toplam = eded1 + eded2;
+//            Console.log(toplam);
+//    }
+    
+
+//   if (eded1 < 50 && eded2 > 50) ;
+// { 
+//     Console.log("eded1=" + eded1 )
+//         Console.log("eded2=" + eded2)
+//             Console.log("Daxil edilen ededlerden biri 50den kicikdir")
+
+// }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////1 false
+
+// var eded1 = 200;
+// var eded2 = 200;
+// var toplam = 0;
+
+// if (eded1 > 50 && eded2 > 50)
+//    {
+//        toplam = eded1 + eded2;
+//            Console.log(toplam);
+//    }
+    
+
+//   if (eded1 < 50 && eded2 > 50) ;
+// { 
+//     Console.log("eded1=" + eded1 )
+//         Console.log("eded2=" + eded2)
+//             Console.log("Daxil edilen ededlerden biri 50den kicikdir")
+
+// }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//3
+// var saygac = qiymet()
+// var ededorta = qiymet()
+// var sum = qiymet()
+// var Mehsullar = [
+//     {
+//         id: qiymet(1),
+//         name: "Honda ",
+//         Model: "civic",
+//         price: qiymet(29000)
+
+//     },
+//         {
+//             id:qiymet(4),
+//             name: "BmW",
+//             Model: "E39 coupe",
+//             price: qiymet(6000)
+
+//         },
+//             {
+//                 id: qiymet(7),
+//                 name: "Mercedes",
+//                 Model: "e230",
+//                 price: qiymet(11000)
+
+//             },
+//                 {
+//                     id: qiymet(11),
+//                     name: "Vaz",
+//                     Model: "2106",
+//                     price: qiymet(2000)
+
+//                 },
+//                     {
+//                         id: qiymet(17),
+//                         name: "Mazda",
+//                         Model: "Mx5",
+//                         price: qiymet (70000)
+
+//                     },
+// ]
+// for (i = 0; i < model.length; i++) {
+//     if (Model[i].id % 2 == 0) {
+//         sum = sum + model[i].price
+        
+//     }
+// }
+// console.log(sum / saygac)
